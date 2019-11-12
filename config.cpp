096a4bc6a799ea62f810f2db85d8f17294386b8a
m3ales <mattfdavies12@gmail.com>
2019-11-12 05:10:04 +0200

    Added dependancies on ace interact/medical, formatted config.cpp

--- config.cpp
+++ config.cpp
@@ -1,34 +1,42 @@
-class CfgPatches
-{
+class CfgPatches {
 	class MIRA_Vehicle_Medical
 	{
 		name = "ACE Vehicle Medical";
 		author = "M3ales";
 		url = "https://github.com/M3ales/MIRA_mods";
-		requiredAddons[] = {};
+		requiredAddons[] = {"ace_interact_menu", "ace_medical", "ace_medical_gui"};
 		units[] = {};
 		weapons[] = {};
 	};
 };
 
-class CfgFunctions
-{
-	class MIRA_Vehicle_Medical{
-        class VehicleMedical{
+class CfgMods {
+    dir = "@MIRA_Vehicle_Medical";
+	name = "ACE Vehicle Medical";
+	picture = "";
+	logo = "";
+	logoOver = "";
+	tooltip = "Arma 3 Typhon";
+	action = "https://github.com/M3ales/MIRA_Vehicle_Medical";
+	overview = "Adds a medical menu accessible from self interact, while inside a vehicle - displays only priority patients who are unstable.";
+}
+
+class CfgFunctions {
+	class MIRA_Vehicle_Medical {
+        class VehicleMedical {
             tag = "MIRA_Vehicle_Medical";
-            requiredAddons[]={};
-            file="MIRA_Vehicle_Medical\functions";
-            class buildPassengerActions{};
-            class buildUnstableActions{};
-            class isBleeding{};
-            class isCardiacArrest{};
-            class isUnconscious{};
+            requiredAddons[] = {"ace_interact_menu",  "ace_medical", "ace_medical_gui"};
+            file = "MIRA_Vehicle_Medical\functions";
+            class buildPassengerActions {};
+            class buildUnstableActions {};
+            class isBleeding {};
+            class isCardiacArrest {};
+            class isUnconscious {};
         };
 	};
 };
 
-class CfgVehicles
-{
+class CfgVehicles {
     class Air;
     class Helicopter: Air {
         class ACE_SelfActions {
