24ed334964fa4b5b19e15733636dae35b915c5c1
m3ales <mattfdavies12@gmail.com>
2019-12-22 22:51:33 +0200

    Fixed #3, Added interaction to a bunch of other vehicle types, cleanup

--- CfgVehicles.hpp
+++ CfgVehicles.hpp
@@ -1,38 +1,42 @@
+#define MEDICAL_BASE(addition) class MIRA_Medical {\
+                displayName = "Medical";\
+                condition = "alive _target";\
+                statement = "if(true)exitWith{}";\
+                ##addition##\
+                class MIRA_Stable {\
+                    displayName = "Stable";\
+                    insertChildren = QUOTE(_this call FUNC(buildStablePassengerActions));\
+                };\
+                class MIRA_Unstable {\
+                    displayName = "Unstable";\
+                    insertChildren = QUOTE(_this call FUNC(buildUnstablePassengerActions));\
+                };\
+            };
 class CfgVehicles {
     class Air;
     class Helicopter: Air {
         class ACE_SelfActions {
-            class MIRA_Medical {
-                displayName = "Medical";
-                condition = "alive _target";
-                statement = "";
-                class MIRA_Stable {
-                    displayName = "Stable";
-                    insertChildren = QUOTE(_this call FUNC(buildStablePassengerActions));
-                };
-                class MIRA_Unstable {
-                    displayName = "Unstable";
-                    insertChildren = QUOTE(_this call FUNC(buildUnstablePassengerActions));
-                };
-            };
+            MEDICAL_BASE( )
         };
     };
-    class Car_F;
-    class Wheeled_APC_F : Car_F {
+    class Plane: Air {
         class ACE_SelfActions {
-            class MIRA_Medical {
-                displayName = "Medical";
-                condition = "alive _target";
-                statement = "";
-                class MIRA_Stable {
-                    displayName = "Stable";
-                    insertChildren = QUOTE(_this call FUNC(buildStablePassengerActions));
-                };
-                class MIRA_Unstable {
-                    displayName = "Unstable";
-                    insertChildren = QUOTE(_this call FUNC(buildUnstablePassengerActions));
-                };
-            };
+            MEDICAL_BASE( )
+        };
+    };
+    class LandVehicle;
+    class Car: LandVehicle {
+        class ACE_SelfActions {
+            MEDICAL_BASE(exceptions[] = {"isNotSwimming"};)
+        };
+    };
+    class Tank: LandVehicle {
+            MEDICAL_BASE(exceptions[] = {"isNotSwimming"};)
+    };
+    class Ship;
+    class Ship_F: Ship {
+        class ACE_SelfActions {
+            MEDICAL_BASE(exceptions[] = {"isNotSwimming"};)
         };
     };
 };
--- functions/fn_needsBandage.sqf
+++ functions/fn_needsBandage.sqf
@@ -1,7 +1,7 @@
 #include "function_macros.hpp"
 params["_unit"];
 _needBandage = false;
-_wounds = _unit call MIRA_Vehicle_medical_fnc_getOpenWounds;
+_wounds = _unit call FUNC(getOpenWounds);
 { 
     _x params ["", "_bodyPartN", "_amountOf", "_bleeding"]; 
     if ((_amountOf * _bleeding) > 0) exitWith { 
