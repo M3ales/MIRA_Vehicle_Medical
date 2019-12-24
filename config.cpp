8ea901446a232134cf57c72d9940d88eb94f532f
m3ales <mattfdavies12@gmail.com>
2019-12-24 21:04:31 +0200

    Config cleanup, added cba_settings as dependency

--- config.cpp
+++ config.cpp
@@ -6,27 +6,16 @@ class CfgPatches {
 		name = QUOTE(ADDON_NAME);
 		author = "M3ales";
 		url = "https://github.com/M3ales/MIRA_Vehicle_Medical";
-		requiredAddons[] = {"ace_interact_menu", "ace_medical"};
+		requiredAddons[] = {"ace_interact_menu", "ace_medical", "cba_settings"};
 		units[] = {};
 		weapons[] = {};
 	};
 };
 
-class CfgMods {
-    dir = QUOTE(CONCAT(@,ADDON));
-	name = QUOTE(ADDON_NAME);
-	picture = "";
-	logo = "";
-	logoOver = "";
-	tooltip = "";
-	action = "https://github.com/M3ales/MIRA_Vehicle_Medical";
-	overview = "Adds a medical menu accessible from self interact, while inside a vehicle - displays only priority patients who are unstable.";
-}
-
 class Extended_PreInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_preInit));
     };
-}; 
+};
 
 #include "CfgVehicles.hpp"
